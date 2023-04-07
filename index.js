abdc0564f237adb0eed4074b5577ae7404b2922a
Gloria <khalayimaloba@gmail.com>
2023-04-08 00:39:16 +0300

    review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Lucia Lemon';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
